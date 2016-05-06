61f745b780337ed316a6571aa35c3b7c0627c6c5
Shane Jackson <shanejackson_2@hotmail.com>
2016-05-06 11:35:43 -0700

    zombies respond to click

--- scripts/zombie.js
+++ scripts/zombie.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
 	// a zombie represented with health and a img on screen
 	// starts "walking" upon instantiation
-	var Zombie = function(health, xPos) {  // this whole thing is a constr. (no class needed b/c that's how js rolls)
+	var Zombie = function(health, xPos, zomNum) {  // this whole thing is a constr. (no class needed b/c that's how js rolls)
 		var moveTimer = null;
 		this.xPos = xPos;
 		this.health = health; 
 		console.log("New zombie with health " + this.health + " in xPos " + xPos);
 		var zombieImage = document.createElement("img");
 		zombieImage.setAttribute('src','images/zombies/zombie0.png');  // establish path for image
+		
 		document.body.appendChild(zombieImage);  		// attach image to body
+		
+		zombieImage.id = zomNum;						// symbolically connects the image to the object
 		zombieImage.style.position = "absolute" 	    // need this or no movement
 		zombieImage.style.top = "-250px";               // img off screen to start
 		zombieImage.style.left = xPos + "px";           // xPos from param   
@@ -18,11 +21,18 @@ $(document).ready(function(){
 				zombieImage.style.top = parseInt(zombieImage.style.top) + 1 + "px";
 				//console.log("In move");
 		}
+		
+		this.ouch = function() {
+			alert(zomNum);
+		}
 		this.moveTimer = setInterval(this.move, 20);    //starts moving
 	}; 
 	
 	// spawn three zombies
-	var z2 = new Zombie(5,0);
-	var z1 = new Zombie(10,150);
-	var z3 = new Zombie(15,300);
+	var zs = new Array();
+	
+	for (i = 0; i < 4; i++) {
+		zs[i] = new Zombie(5, i * 200, i);
+		document.getElementById(i).onclick = zs[i].ouch;
+	}
 });
