6ffd5bbe179a481fd3f5d28fbad1b37e8835ec77
Chicadas <vincentlam060@gmail.com>
2016-05-07 18:40:58 -0700

    bulletQueue.js working with boostrap.html

--- scripts/bulletQueue.js
+++ scripts/bulletQueue.js
@@ -1,28 +1,33 @@
-<script>
-
-var bulletQueueValues = new Array(10);
-var queue0 = document.getElementById
+	var bulletQueueValues = new Array(5);
 
 	function generateQueue(){
-		var queue = document.getElementById(queue);
-		for(i = 0; i < 10; i++){
+		for(i = 0; i < 5; i++){
 			bulletQueueValues[i] = Math.floor((Math.random() *21) - 10);  
-			document.getElementsById("queue" + i).innerHTML = bulletQueueValues[i]
+			document.getElementById("queue" + i).innerHTML = bulletQueueValues[i];
 		}
 	}
 	
 	function addRandomBullet(){
+		var length = bulletQueueValues.length;
 		var x = Math.floor((Math.random() * 21) - 10);
 		bulletQueueValues.shift();
 		bulletQueueValues.push(x);
+		for(i = 0; i < length; i++){
+			document.getElementById("queue" + i).innerHTML = bulletQueueValues[i];
+		}		
 	}
 	
-	function printQueue(){
-	var length = bulletQueueValues.length;
-		for(i = 0; i < length; i++){
-			document.getElementById("output").innerHTML += "<br/>" +  bulletQueueValues[i];  
+	function updateRandomBullet() {
+		if(bulletQueueValues[0] == undefined){
+			generateQueue();
 		}
-			document.getElementById("output").innerHTML += "<br/>"; 
+		addRandomBullet();
 	}
-
-</script>
+	
+	$(document).ready(function() {
+	var queue = document.getElementById('queue0');
+	
+	queue.onclick = function() {
+		updateRandomBullet();
+	}
+});
