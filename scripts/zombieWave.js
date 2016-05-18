7609a7f34fbc084a16ed6308fbde9e19684b3e87
Shane Jackson <shanejackson_2@hotmail.com>
2016-05-17 21:36:02 -0700

    shoving

--- scripts/zombieWave.js
+++ scripts/zombieWave.js
@@ -1,7 +1,7 @@
 /*
 zombie as represented with health and a img on screen
 starts "walking" upon instantiation.
-*/
+ */
 
 // holds all currently active zombies
 var zs = new Array();
@@ -29,7 +29,7 @@ $(document).ready(function(){
 	 zomNum Unique number to identify an individual zombie
 	 		(used as ID tag for its div)
 	 yPos   y position of the zombie 
-	*/
+	 */
 	var Zombie = function(health, xPos, zomNum, yPos) {  
 		var zomNum = zomNum;
 		var xPos = xPos;
@@ -38,21 +38,21 @@ $(document).ready(function(){
 		var health = health;
 		// message at construction
 		console.log("# " + zomNum + " health: " + health + " xPos: " + xPos);
-		
+
 		// holds timer for movement
 		var moveTimer = null;
 		// holds timer for animation
 		var animateTimer = null;
 		// holds image number
 		var imageNumber = 0;
-		
+
 		// creates zombie image element
 		var zombieImage = document.createElement("img");
 		// creates zombie image and health elements
 		var zombieHolder = document.createElement("div");
 		// creates zombie health elements
 		var zombieHealthText = document.createElement("div");
-		
+
 		//set styles for container for zombie and health 
 		zombieHolder.style.height = "40%";
 		zombieHolder.style.maxHeight = "188px";
@@ -63,7 +63,7 @@ $(document).ready(function(){
 
 		// assign id for holder for zombie and health elemets
 		zombieHolder.id = zomNum;
-		
+
 		//set styles for container health 
 		zombieHealthText.innerHTML = health;
 		zombieHealthText.style.textAlign = "center";
@@ -72,7 +72,7 @@ $(document).ready(function(){
 		zombieHealthText.style.position = "relative";
 		zombieHealthText.style.height = "20%";
 		zombieHealthText.style.top = "-100%";
-		
+
 		//set styles for zombie image 
 		zombieImage.id = zomNum + "zImage";
 		zombieImage.src = "images/zombies/zombie0.png";
@@ -83,24 +83,24 @@ $(document).ready(function(){
 		zombieImage.style.marginLeft = "auto";
 		zombieImage.style.marginRight = "auto";
 		zombieImage.style.zIndex = "1";
-		
+
 		//adding health text and zombie image to zombieHolder
 		zombieHolder.appendChild(zombieHealthText);
 		zombieHolder.appendChild(zombieImage);	
-		
+
 		//adding zombieHolder to screen
 		document.getElementById("lawn").appendChild(zombieHolder);
-		
+
 		/*
 		returns true if zombie has caused game over 
-		*/
+		 */
 		function atDotted() {
 			return yPos >= 100;
 		} 
-		
+
 		/*
 		Causes the image to move down the screen until it hits the dotted line 
-		*/
+		 */
 		this.move = function() { 
 			if (atDotted()){
 				// clears the movement
@@ -120,12 +120,12 @@ $(document).ready(function(){
 				zombieHolder.style.top = yPos + "%";
 			}
 		}
-		
+
 		/*
 		"kills" a zombie by clearing the intervals
 		and removing it from the array that holds zombies
 		Used when swtiching into the Easter Egg mode
-		*/
+		 */
 		this.wipe = function() {
 			//stops movement
 			clearInterval(moveTimer);
@@ -140,12 +140,12 @@ $(document).ready(function(){
 				document.getElementById(zomNum).remove();
 			}
 		}		
-		
+
 		/*
 		"kills" the zombie 
-		*/
+		 */
 		function die() {
-			
+
 			console.log("die");
 			killCount++;
 			//stops the zombie from calling move/animate functions
@@ -173,7 +173,7 @@ $(document).ready(function(){
 				fadeStatus = false;
 				// calls fade aimation 
 				if(disable == false) {
-				fade();
+					fade();
 				}
 				if(wave == 4) {
 					fadeEnd();
@@ -186,33 +186,33 @@ $(document).ready(function(){
 					spawnNum++;
 					// starts the next wave 
 					if(wave == 1) {
-					callWaveFirst(spawnNum);
+						callWaveFirst(spawnNum);
 					}
 					if(wave >=2 && wave <=2) { 
 						callWaveSecond(spawnNum);
-						alert("second betch");
+						//alert("second betch");
 					}
 					if(wave >=3 && wave <=3) { 
 						callWaveFinal(spawnNum);
-						alert("final betch");
+						//alert("final betch");
 					}
 				}
 			}
 		}		
-		
+
 		/*
 		animates the image ie. makes it "walk"
-		*/
+		 */
 		this.animate = function() {
 			imageNumber = (imageNumber + 1) % 2;
 			var imageName = "images/zombies/zombie" + imageNumber + ".png";
 			zombieImage.setAttribute('src', imageName);
 		}
-		
+
 		/*
 		handler for onclick behavoir, if zombie's health is 0, it dies
 		 else, health is changed
-		*/
+		 */
 		this.hit = function(){
 			checkGun();
 			updateRandomBullet();
@@ -226,12 +226,12 @@ $(document).ready(function(){
 						+ " health: " + health);
 			}			
 		}
-		
+
 		/*
 		performs a math operation depending on which gun is selected
-		*/
+		 */
 		function checkGun() {
-		  //checks gun selected
+			//checks gun selected
 			if(selectedGun == 1) {
 				//plus gun
 				plusOperation();
@@ -247,52 +247,52 @@ $(document).ready(function(){
 				diviOperation();
 			}
 		}
-		
+
 		/*
 		adds
-		*/
+		 */
 		function plusOperation() {
 			health = health + currentBullet;
 			console.log("new health: " + health);
 		}
-		
+
 		/*
 		subtracts
-		*/
+		 */
 		function minusOperation() {
 			health = health - currentBullet;
 			console.log("new health: " + health);
 		}
-		
+
 		/*
 		multiplies
-		*/
+		 */
 		function multiOperation() {
 			health = health * currentBullet;
 			console.log("new health: " + health);
 		}
-		
+
 		/*
 		divides (rounds up)
 		checks if easter egg is triggered 
 		when the user divides by zero,
 		else, divides normally
-		*/
+		 */
 		function diviOperation() {		
-		if(currentBullet == 0) {
-			if (easterEggThisWave){				
-				score += 5;
-				triggerEasterEgg();	
+			if(currentBullet == 0) {
+				if (easterEggThisWave){				
+					score += 5;
+					triggerEasterEgg();	
 				}
 			} else {
 				health = Math.ceil(health / currentBullet);
 				console.log("new health: " + health);
 			}
 		}
-	
+
 		/*
 		sets up the easter egg
-		*/
+		 */
 		function triggerEasterEgg() {
 			//sending player vars
 			carryVars();
@@ -306,38 +306,38 @@ $(document).ready(function(){
 			c[4].parentElement.removeChild(c[4]);
 			//adds pony script
 			var fileref=document.createElement('script')
-			fileref.setAttribute("src", "scripts/pony.js");
+					fileref.setAttribute("src", "scripts/pony.js");
 			document.getElementsByTagName("head")[0].appendChild(fileref);
 		}
-		
+
 		/*
 		stops movement when pause clicked
-		*/
+		 */
 		this.stopMove = function() {
-		  clearInterval(moveTimer);
-		  moveTimer = null;
-		  clearInterval(animateTimer);
-		  animateTimer = null;
+			clearInterval(moveTimer);
+			moveTimer = null;
+			clearInterval(animateTimer);
+			animateTimer = null;
 		}
-		
+
 		/*
 		starts movement after pause
-		*/
+		 */
 		this.startMove = function() {
-		  moveTimer = setInterval(this.move, 10);  
-		  animateTimer = setInterval(this.animate, 800);		
+			moveTimer = setInterval(this.move, 10);  
+			animateTimer = setInterval(this.animate, 800);		
 		}
-	
+
 		//auto caller for moving 
 		moveTimer = setInterval(this.move, 10);
 		//auto caller for animating
 		animateTimer = setInterval(this.animate, 800);
 	};
 	// ___________________________________________________ zombie constr ends 
-	
+
 	/*
 	kills all zombies
-	*/
+	 */
 	function killAll() {
 		for (j = 0; j < zs.length; j++) {
 			if (zs[j] != null) {
@@ -348,10 +348,10 @@ $(document).ready(function(){
 			}
 		}
 	}
-	
+
 	/*
 	random num helper for first section of waves for zombie health 
-	*/ 
+	 */ 
 	function healthFirst() {
 		out = Math.floor((Math.random() * 5) + 1);
 		if ((Math.random() * 2) > 1) {
@@ -362,7 +362,7 @@ $(document).ready(function(){
 	}
 	/*
 	random num helper for second section of waves for zombie health 
-	*/ 
+	 */ 
 	function healthSecond() {
 		out = Math.floor((Math.random() * 10) + 1);
 		if ((Math.random() * 2) > 1) {
@@ -373,7 +373,7 @@ $(document).ready(function(){
 	}
 	/*
 	random num helper for final section of waves for zombie health 
-	*/ 
+	 */ 
 	function healthFinal() {
 		out = Math.floor((Math.random() * 15) + 1);
 		if ((Math.random() * 2) > 1) {
@@ -384,23 +384,23 @@ $(document).ready(function(){
 	}
 	/*
 	random num helper for xPos 
-	*/ 
+	 */ 
 	function xRandom() {
 		return Math.floor(Math.random() * 4) * 25; 
 	}
 
 	/*
 	random num helper for yPos 
-	*/ 
+	 */ 
 	function yRandom() {
 		return Math.floor(((Math.random() * 150) + 50) * -1); 
 	}
-	
+
 	//holds number of zombies that are spawned
 	var spawnNum = 1;
 	/*
 	spawns spawnNum zombies
-	*/
+	 */
 	function callWaveFirst(spawnNum){
 		for (i = 0; i < spawnNum; i++) {
 			zs[i] = new Zombie(healthFirst(), xRandom(), i, yRandom() );  
@@ -426,17 +426,16 @@ $(document).ready(function(){
 	callWaveFirst(spawnNum);
 	// flag for fading 
 	var fadeStatus;
-	
+
 	// handles fading animation 
 	function fade() {
 		$("#NW").fadeIn(3000);
 		$("#NW").fadeOut(3000);
-		alert(wave);
 		fadeStatus = true;
 	}
 	function fadeEnd() {
 		$("#youWin").fadeIn(3000);
 		document.location.href = 'youWin.html?';
-		
+
 	}
 });
