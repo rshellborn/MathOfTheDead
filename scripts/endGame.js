0f3154e49dcef165d87f446e3aea351bb0bf0d79
Andrew Busto <aBusto@shaw.ca>
2016-05-23 17:32:14 -0700

    separating some scripts from html pages

---
+++ scripts/endGame.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	// gets current wave number
+	var wave = getSessionItem("wave");
+	// gets the current score value
+	var score = getSessionItem("score");
+	// gets the player's name
+	var name = getSessionItem("name");
+	//gets player ID
+	var id = getSessionItem("id");
+	
+	var pagePath = location.pathname;
+	
+	if (pagePath.substring(pagePath.length - 14) == "endOfGame.html") {
+		// audio 
+		zWantBrains.play(); 
+		playerDie.play(); 
+		
+		//prints the wave number onto the top bar of the screen
+		document.getElementById("wave").textContent=("Wave " + wave);	
+	}
+	
+	//prints the wave number onto the top bar of the screen
+	document.getElementById("score").textContent=("Score: " + score);
+	
+	//sends player info to database
+	if (score != null) {
+		$.ajax( { url: "https://api.mlab.com/api/1/databases/mathofthedead/collections/babysfirstdb?&apiKey=ArvUzWX9LKcVcKGcpfgPMVS62vXkF_Xu&f=" + JSON.stringify({"_id" : 0}),
+			data: JSON.stringify( {"playerID": "not_used", "score": parseInt(score), "wave": parseInt(wave), "name": name } ),
+			type: "post",
+			contentType: "application/json; charset=utf-8",
+		});
+	}
+	
+	//triggers achievement
+	if(getSessionItem("10wave") == 1) {
+		triggerAchievement();
+	}
+	
+	//clears data from the game to allow replay
+	removeSessionItem("wave");
+	removeSessionItem("score");
+});
---
+++ scripts/sendName.js
@@ -0,0 +1,28 @@
+/*
+after player inputs name and clicks start, this proceeds to the game
+*/
+function sendName(setMode) {
+	//sets player variables based on input
+	var name = document.getElementById("nameInput").value;
+	var id = randomID();
+	console.log(name);
+		
+	//removes session items to reset them
+	removeSessionItem("id");
+	removeSessionItem("name");
+		
+	//creates session items for player variables
+	createSessionItem("id", id);
+	createSessionItem("name", name);
+		
+	//sets the game mode based on input
+	var mode = setMode;
+	
+	//resets session item mode
+	removeSessionItem("mode", mode);
+	createSessionItem("mode", mode);
+	
+	console.log('mode set to: ' + mode);
+	
+	fadeEnd("gameScreen.html");
+}
