3f805b809f0268759d1f9219543a9028ffca44fd
Andrew Busto <aBusto@shaw.ca>
2016-05-24 13:45:49 -0700

    zombies in background of main menu

---
+++ scripts/backgroundZombies.js
@@ -0,0 +1,204 @@
+/*
+  Zombie as represented with health and a img on screen
+  starts "walking" upon instantiation.
+*/
+$(document).ready(function(){	
+	/* --------------------------------------START OF bgZombie Object------------------------------------- */
+
+	/*
+	 Constructs a zombie with a health, x position and y position, and a number
+	 @params 
+	 health Health of the zombie
+	 xPos 	x position of the zombie
+     zomNum Unique number to identify an individual zombie (used as ID tag for its div)
+	 yPos   y position of the zombie 
+	*/
+	var bgZombie = function(zomNum, health, xPos, yPos) { 
+		var zomNum = zomNum;
+		var xPos = xPos;
+		var yPos = yPos;
+		var health = health;
+		
+		// Speed of the zombie
+		var speed = 0.03;
+		// Used the calculate score that will be awarded for killing this zombie
+		var maxHealth = Math.abs(health);
+		
+		//Message to show the zombie has been spawned.
+		console.log("fake# " + zomNum + " health: " + health + " xPos: " + xPos + " yPos: " + yPos);
+		
+		// Holds timer for movement
+		var moveTimer = null;
+		// Holds timer for animation
+		var animateTimer = null;
+		// Holds image number
+		var imageNumber = 0;
+		
+		// Creates zombie image element
+		var zombieImage = document.createElement("img");
+		// Creates zombie image and health elements
+		var zombieHolder = document.createElement("div");
+		// Creates zombie health elements
+		var zombieHealthText = document.createElement("div");
+		
+		
+		/* ----------------------------------START OF Zombie Styling-------------------------------------- */
+
+		// Set styles for container for zombie and health 
+		zombieHolder.style.height = "20%";
+		zombieHolder.style.maxHeight = "150px";
+		zombieHolder.style.width = "25%";
+		zombieHolder.style.position = "absolute";
+		zombieHolder.style.top = yPos + "%"; 
+		zombieHolder.style.left = xPos + "%";
+
+		// Assign id for holder for zombie and health elemets
+		zombieHolder.id = zomNum;
+		
+		// Set styles for container health 
+		zombieHealthText.innerHTML = health;
+		zombieHealthText.style.textAlign = "center";
+		zombieHealthText.style.color = "White";
+		zombieHealthText.style.fontSize = "150%";
+		zombieHealthText.style.position = "relative";
+		zombieHealthText.style.height = "20%";
+		zombieHealthText.style.top = "-100%";
+		
+		// Set styles for zombie image 
+		zombieImage.id = zomNum + "zImage";
+		zombieImage.src = "images/zombies/zombie0.png";
+		zombieImage.style.height = "100%";
+		zombieImage.style.position = "relative";
+		zombieImage.style.display = "block";
+		zombieImage.style.top = "-100%";
+		zombieImage.style.marginLeft = "auto";
+		zombieImage.style.marginRight = "auto";
+		
+		// Adding health text and zombie image to zombieHolder
+		zombieHolder.appendChild(zombieImage);
+		zombieHolder.appendChild(zombieHealthText);	
+		
+		//adding zombieHolder to screen
+		document.getElementsByClassName("row specialrow")[0].appendChild(zombieHolder);
+		
+		/* ----------------------------------END OF bgZombie Styling-------------------------------------- */
+		
+		
+		
+		/* ----------------------------------------START OF Zombie Movement------------------------------------------ */
+		
+		/*
+		Causes the image to move down the screen until it hits the dotted line.
+		*/
+		this.move = function() { 
+			/* Increments y position to move the zombie downwards */
+			yPos += speed;
+			zombieHolder.style.top = yPos + "%";
+			
+			if (yPos >= 120) {
+				yPos = -20; 
+				xPos = xRandom();
+				zombieHolder.style.left = xPos + "%";
+			}
+		}
+		
+		/*
+			Animates the walk by switching between two images of the zombie
+		*/
+		this.animate = function() {
+			imageNumber = (imageNumber + 1) % 2;
+			var imageName = "images/zombies/zombie" + imageNumber + ".png";
+			zombieImage.setAttribute('src', imageName);
+		}
+		
+		/*
+			Stops all zombie movement
+		*/
+		this.stopMove = function() {
+		  clearInterval(moveTimer);
+		  moveTimer = null;
+		  clearInterval(animateTimer);
+		  animateTimer = null;
+		}
+		
+		/*
+			Starts all zombie movement
+		*/
+		this.startMove = function() {
+		  moveTimer = setInterval(this.move, 10);  
+		  animateTimer = setInterval(this.animate, 1000);		
+		}
+		
+		//auto caller for moving 
+		moveTimer = setInterval(this.move, 10);
+		//auto caller for animating
+		animateTimer = setInterval(this.animate, 1000);
+		
+		/* ----------------------------------------END OF Zombie Movement------------------------------------------ */
+		
+				
+		/* ----------------------------------------START OF Killing Zombies------------------------------------------ */
+		
+	 	/* 
+			Removes zombie from the screen and deletes all references to it.
+		*/
+		this.remove = function() {
+			//stops movement
+			clearInterval(moveTimer);
+			moveTimer = null;			
+			//stops animation
+			clearInterval(animateTimer);
+			animateTimer = null;			
+			//removes the zombie from the array
+			zs[zomNum] = null;
+			//removes the image from the screen
+			if (document.getElementById(zomNum) != null){
+				document.getElementById(zomNum).remove();
+			}
+		}	
+		
+		/* ----------------------------------------END OF Killing Zombies------------------------------------------ */
+	
+	};
+	/* --------------------------------------END OF Zombie Object------------------------------------- */
+	
+	/*
+	generates BackgroundZombies
+	*/
+	function makeBGZombies() {
+		var bgZombies = new Array();
+		for (var i = 0; i < 5; i++) {
+			bgZombies[i] = new bgZombie(i, genHealth(), xRandom(), ((i * -25) - 20));
+		}
+	}
+	
+	/* ----------------------------START OF Random and Generation Functions-------------------------- */
+	
+	/*
+	random num helper for zombie health 
+	 */ 
+	function genHealth() {
+		var out = Math.floor((Math.random() * 10) + 1);
+		if ((Math.random() * 2) > 1) {
+			return out * -1;
+		} else {
+			return out;
+		}
+	}
+	
+	/*
+		Random num helper for xPos 
+	*/ 
+	function xRandom() {
+		return Math.floor(Math.random() * 4) * 25; 
+	}
+
+	/*
+		lanePlacement ensures multiple zombies will not spawn on top of eachother on the x axis
+	*/
+	function lanePlacement(laneNum) {
+		return Math.floor(laneNum) * 25;  
+	}
+	/* ----------------------------END OF Random and Generation Functions-------------------------- */
+	makeBGZombies();
+});
