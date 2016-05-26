999c98478009d259b5040e6e76e6b0168cf64797
mfisli <mfisli2@gmail.com>
2016-05-26 14:09:11 -0700

    commending audio socrecs

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,17 +1,19 @@
 /* holds audio objects */ 
 console.log("audio loaded");
 
-// main menu
+/* main menu */
 var menuIntro = new Audio("audio/menuIntro.mp3");
+// https://soundcloud.com/horror-sound-design/alien-glimpse-scare-3
 var birds = new Audio("audio/birds.mp3");
 // http://soundbible.com/1818-Rainforest-Ambience.html
 var bird = new Audio("audio/bird.mp3");
 // http://anything2mp3.com/system/files/mstr1/birds013_birds013_generic.mp3
 var menuBGM = new Audio("audio/menuBGM.mp3");
+// https://soundcloud.com/georgevlad/neighbourhood-ambience
 
-// pony
-// https://youtu.be/ZcBNxuKZyN4
+/* pony */
 var MLPMusic = new Audio("audio/music/MLPMusic.mp3");
+// https://youtu.be/ZcBNxuKZyN4
 
 // game-screen music
 var bgmWave = new Audio("audio/music/MichaelMyers.mp3");
@@ -19,27 +21,33 @@ var bgmWave = new Audio("audio/music/MichaelMyers.mp3");
 var bgmInfini = new Audio("audio/music/BroForce.mp3");
 // https://youtu.be/evxEH0m_WZI
 
-// game over 
+/* game over */
 var zWantBrains = new Audio("audio/zWantBrains.mp3");
 // http://soundbible.com/1027-Zombies-Eat-Brains.html
 var playerDie = new Audio("audio/playerDie.mp3");
 // http://soundbible.com/1033-Zombie-In-Pain.html
 
-// bullet queue red button 
+/* bullet queue red button */
 var nextBullet = new Audio("audio/nextBullet.mp3"); 
 // http://soundbible.com/1988-Gun-Cocking-Fast.html
 
-// next wave starts
+/* next wave starts */
 var nextWave = new Audio("audio/nextWave.mp3"); 
+// https://soundcloud.com/emma-collins-21/horror-ghostly-breath-effect
 
-// zombies 
+/* zombies */ 
 var zDie = new Audio("audio/zDie.mp3"); 				//die
 // http://soundbible.com/1029-Mindless-Zombie-Awakening.html
-var zStillAlive = new Audio("audio/zStillAlive.mp3");   //hit but alive 
+var zStillAlive = new Audio("audio/zStillAlive.mp3");   //hit but alive
+// http://soundbible.com/1413-Strange-Growl.html
 var shot = new Audio("audio/shot.mp3"); 				//shot gun sound
+// http://soundbible.com/2095-Mossberg-500-Pump-Shotgun.html
 
-// guns select click 
+
+/* guns select click */
 var gunSelect = new Audio("audio/gunSelect.mp3");
+// http://soundbible.com/947-Metal-Bang.html
 
-// achievement unlocked
+/* achievement unlocked */ 
 var achivUnlocked = new Audio("audio/Cha_Ching.mp3")
+// http://soundbible.com/1997-Cha-Ching-Register.html
